a385a55a634dbeed2741defe2756452faecf4e19
Abhishek.Arun <abhishek.arun@abhishearunsmbp.clients.asia-pac.shell.com>
2019-11-21 11:59:07 +0530

    sdsdf

--- 001/main.tf
+++ 001/main.tf
@@ -3,7 +3,7 @@ default = "and now most of them are polluted or dry."
 }
 
 module "eswarmodule"{
- source = "git@github.com:eswarkp/terraform-training.git"
+ source = "git@github.com:eswarkp/terraform-training.git?ref=v0.0.2"
 }
 output "result" {
  value = "${module.eswarmodule.story} ${var.story}"
