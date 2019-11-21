679622614bbafecd6259983acee55e3de9b7975c
Abhishek.Arun <abhishek.arun@abhishearunsmbp.clients.asia-pac.shell.com>
2019-11-21 11:38:15 +0530

    new commit

--- 001/main.tf
+++ 001/main.tf
@@ -5,6 +5,6 @@ default = "and now most of them are polluted or dry."
 module "eswarmodule"{
  source = "git@github.com:eswarkp/terraform-training.git"
 }
-output "story" {
+output "result" {
  value = "${module.eswarmodule.story} ${var.story}"
 }
